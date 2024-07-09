f5b8f572e883e68aaf55de30892c8efff93c7c95
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-07-09 14:11:31 +0530

    learnt about object destructuring in js

--- app.js
+++ app.js
@@ -27,17 +27,19 @@ const Header = () => {
 }
 
 const RestaurantCard = (props) => {
-    console.log(props)
+    const {resName,cuisine,rating,eta} = props
+    console.log(resName);
     return (
         <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
             <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/4fe928562d04e0a5c25f335e866dad71"/>
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisine}</h4>
-            <h4>{props.rating} Stars</h4>
-            <h4>{props.eta} mins</h4>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>{rating} Stars</h4>
+            <h4>{eta} mins</h4>
         </div>
     )
 }
+
 const Body = () => {
     return(
         <div className="body">
@@ -49,6 +51,7 @@ const Body = () => {
         </div>
     )
 }
+
 const AppLayout = () => {
     return(
         <div className="app">
