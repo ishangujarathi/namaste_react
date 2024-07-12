b6139bdfee61577291a0499c846c665fb9d0d77a
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-07-12 16:27:14 +0530

    refactored the code, added the mock data and tried to showcase it on the UI using the map function

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,17 @@
 import RestaurantCard from "./Restaurantcard";
+import resList from "../utils/mockData";
 
 const Body = () => {
-    return(
-        <div className="body">
-            <div className="filter">
-                <button className="filter-btn" onClick={()=>{
-                    console.log("Button Clicked");
-                }}>
-                    Top Rated Restaurant
-                </button>
-            </div>
-            <div className="rest-container">
-                <RestaurantCard resName="Meghana Foods" cuisine="Biryanis, North Indian, South Indian" rating="4.4" eta="30"/>
-                <RestaurantCard resName="KFC" cuisine="Burger,Winger Box, Coak" rating="4.2" eta="30"/>
-            </div>
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          {resList.map((restaurant) => (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          ))}
         </div>
-    )
-}
+      </div>
+    );
+  };
 
 export default Body;
