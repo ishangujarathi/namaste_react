60566539353694820821dd95fcb8a7534850cfc7
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-07-15 17:39:19 +0530

    started with a few css changes to the res cards. Got introduced to React hooks, which are nothing but just JS Utility functions which are used to create State Variables. Used the useState() hook and rendered the restaurant cards and also filtered the restairant cards and re-rendered the UI withstate varible changes, react re-renders the entire component and it does it so fast, quaickly triggers the DIFF algorithm, which calculates the difference between the 2 DOMs and updates the UI. This overall algorithm is also known as React Fiber

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestaurantCard from "./Restaurantcard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+
+  //Local State Variable -> Super Powerful Variable
+  const [listofRestaurants,setlistofRestaurants] = useState(resList);
+  
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn" onClick={() => {
+            const filteredlist = listofRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setlistofRestaurants(filteredlist);
+            console.log(listofRestaurants);
+          }}>Top Rated Restaurants</button>
+        </div>
         <div className="res-container">
-          {resList.map((restaurant) => (
+          {listofRestaurants.map((restaurant) => (
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
           ))}
         </div>
