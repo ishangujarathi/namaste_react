9dd017d6934c7b568d54b9a330651d7ba86e1423
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-07-30 17:30:05 +0530

    started with class based components, learnt about how to create class based components, difference between class and functional components and learnt to pass props into class based components

---
+++ src/components/UserClass.js
@@ -0,0 +1,19 @@
+import React from "react"
+class UserClass extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render()
+    {
+        const {name, location} = this.props;
+        return(
+            <div className="user-card">
+                <h2>Name: {name}</h2>
+                <h3>Location: {location}</h3>
+                <h4>Contact: @ishangujarathi</h4>
+            </div>
+        );
+    }
+}
+
+export default UserClass;
