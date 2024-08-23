296f1af57f6a2723045e2db54d381a416baf0e65
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-08-23 12:39:26 +0530

    Instead of writing test cases with test keywor, we can use it keyword as well, because it is just an alias of test and industry standard as well

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,25 +3,25 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact Us Component Test Cases", ()=>{
-    test("Should Load Contact Us Component",()=>{
+    it("Should Load Contact Us Component",()=>{
         render(<Contact/>);
         const heading = screen.getByRole("heading");
         expect(heading).toBeInTheDocument();
     });
     
-    test("Should Load Button inside Contact Component",()=>{
+    it("Should Load Button inside Contact Component",()=>{
         render(<Contact/>);
         const button = screen.getByRole("button");
         expect(button).toBeInTheDocument();
     });
     
-    test("Should Load input name inside Contact Component",()=>{
+    it("Should Load input name inside Contact Component",()=>{
         render(<Contact/>);
         const input = screen.getByPlaceholderText("Your Name");
         expect(input).toBeInTheDocument();
     });
     
-    test("Should load 3 input boxes on the Contact Component",()=>{
+    it("Should load 3 input boxes on the Contact Component",()=>{
         render(<Contact/>);
         const input_boxes = screen.getAllByRole("textbox");   //known as querying, i.e we are querying on the screen
         //console.log(input_boxes.length);  returns JSX element, which is React Element, which is Object at the end of the day
