fd61a4f345e086bf448244ebfff9a1b46259bee7
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-08-23 12:33:42 +0530

    added all the test cases inside a describe block, which again takes 2 parameters, description of the block and an arrow function. Conventional way to group all test cases inside a single block

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,27 +2,29 @@ import { render,screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should Load Contact Us Component",()=>{
-    render(<Contact/>);
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-});
-
-test("Should Load Button inside Contact Component",()=>{
-    render(<Contact/>);
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-});
-
-test("Should Load input name inside Contact Component",()=>{
-    render(<Contact/>);
-    const input = screen.getByPlaceholderText("Your Name");
-    expect(input).toBeInTheDocument();
-});
-
-test("Should load 3 input boxes on the Contact Component",()=>{
-    render(<Contact/>);
-    const input_boxes = screen.getAllByRole("textbox");   //known as querying, i.e we are querying on the screen
-    //console.log(input_boxes.length);  returns JSX element, which is React Element, which is Object at the end of the day
-    expect(input_boxes.length).toBe(3);
+describe("Contact Us Component Test Cases", ()=>{
+    test("Should Load Contact Us Component",()=>{
+        render(<Contact/>);
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+    });
+    
+    test("Should Load Button inside Contact Component",()=>{
+        render(<Contact/>);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("Should Load input name inside Contact Component",()=>{
+        render(<Contact/>);
+        const input = screen.getByPlaceholderText("Your Name");
+        expect(input).toBeInTheDocument();
+    });
+    
+    test("Should load 3 input boxes on the Contact Component",()=>{
+        render(<Contact/>);
+        const input_boxes = screen.getAllByRole("textbox");   //known as querying, i.e we are querying on the screen
+        //console.log(input_boxes.length);  returns JSX element, which is React Element, which is Object at the end of the day
+        expect(input_boxes.length).toBe(3);
+    });
 })
