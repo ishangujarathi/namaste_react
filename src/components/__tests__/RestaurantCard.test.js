802469693f558e575ebbb91d162aec82903d3209
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-08-24 16:33:07 +0530

    wrote some more unit tests for header component

---
+++ src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react"
+import RestaurantCard from "../Restaurantcard"
+
+
+it("Should render ResturantCard component with props data",()=>{
+    render(<RestaurantCard/>)
+})
